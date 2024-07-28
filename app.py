1d6b310b52961ae921382cbc0ba419b36cda7604
Your Name <you@example.com>
2024-07-28 22:05:10 +0800

    feat: Add support for predicting Katakana characters

--- app.py
+++ app.py
@@ -9,15 +9,25 @@ from werkzeug.datastructures import FileStorage
 import os
 
 from Hiragana.hiraganajapanese import label as hiragana_label
+from Katakana.katakanajapanese import label as katakana_label
 
 app = Flask(__name__)
 CORS(app)
 
 # absolute path to the model file
 abs_path = os.path.abspath(os.path.dirname(__file__))
-model_path = os.path.join(abs_path, "Hiragana/hiragana.h5")
-print(model_path)
-model = keras.models.load_model(model_path)
+model_path1 = os.path.join(abs_path, "Hiragana/hiragana.h5")
+model_path2 = os.path.join(abs_path, "Katakana/katakana.h5")
+
+hira_model = keras.models.load_model(model_path1)
+kata_model = keras.models.load_model(model_path2)
+
+
+def invert_image(image):
+    gray_image = ImageOps.grayscale(image)
+    inverted_image = ImageOps.invert(gray_image)
+    inverted_image.save("./uploads/inverted_image.png")
+    return inverted_image
 
 def preprocess_image(image_file):
     img = Image.open(image_file).convert("L") 
@@ -26,20 +36,21 @@ def preprocess_image(image_file):
     img_reshaped = img_normalized.reshape(1, 48, 48, 1)
     return img_reshaped
 
-
-def predict_hiragana(image):
+def predict_charater(char_type, image):
     processed_image = preprocess_image(image)
+    if char_type == "hiragana":
+        model = hira_model
+        label = hiragana_label
+    elif char_type == "katakana":
+        model = kata_model
+        label = katakana_label
+    else:
+        return "Invalid character type", 0.0
+
     prediction = model.predict(processed_image)
     predicted_index = np.argmax(prediction)
-    predicted_hiragana = hiragana_label[predicted_index]
-    return predicted_hiragana, float(prediction[0][predicted_index])
-
-
-def invert_image(image):
-    gray_image = ImageOps.grayscale(image)
-    inverted_image = ImageOps.invert(gray_image)
-    inverted_image.save("./uploads/inverted_image.png")
-    return inverted_image
+    predicted_charater = label[predicted_index]
+    return predicted_charater, float(prediction[0][predicted_index])
 
 
 @app.route("/", methods=["GET"])
@@ -50,6 +61,8 @@ def index():
 def predict():
     if "image" not in request.files:
         return jsonify({"error": "No image file provided"}), 400
+
+    char_type = request.form.get("char_type")
 
     image_file = request.files["image"]
     if image_file.filename == "":
@@ -69,7 +82,7 @@ def predict():
                 content_type=image_file.content_type,
             )
 
-            predicted_char, confidence = predict_hiragana(processed_image_file)
+            predicted_char, confidence = predict_charater(char_type, processed_image_file)
 
             print(predicted_char, confidence)
             return jsonify(
